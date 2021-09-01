b1fbfdfd3b28f45f04945852b1960d2fcd7bb058
André Lamelas <andre@lamelas.org>
2021-09-01 15:52:17 +0100

    bogh

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -2,7 +2,6 @@ import {
   AccessoryConfig,
   AccessoryPlugin,
   API,
-  Characteristic,
   CharacteristicEventTypes,
   CharacteristicGetCallback,
   CharacteristicSetCallback,
@@ -16,7 +15,8 @@ import { connect, MqttClient } from 'mqtt';
 /*
  * IMPORTANT NOTICE
  *
- * One thing you need to take care of is, that you never ever ever import anything directly from the "homebridge" module (or the "hap-nodejs" module).
+ * One thing you need to take care of is, that you never ever ever import anything directly from the "homebridge" module (or the
+ * "hap-nodejs" module).
  * The above import block may seem like, that we do exactly that, but actually those imports are only used for types and interfaces
  * and will disappear once the code is compiled to Javascript.
  * In fact you can check that by running `npm run build` and opening the compiled Javascript file in the `dist` folder.
@@ -76,7 +76,7 @@ export class SonoffRFBridgeAccessory implements AccessoryPlugin {
         callback(undefined, this.state);
       })
       .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
-        if (this.state == 2) {
+        if (this.state === 2) {
           this.serviceBaixo.setCharacteristic(api.hap.Characteristic.On, false);
         }
 
@@ -93,7 +93,7 @@ export class SonoffRFBridgeAccessory implements AccessoryPlugin {
         callback(undefined, this.state);
       })
       .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
-        if (this.state == 1) {
+        if (this.state === 1) {
           this.serviceCima.setCharacteristic(api.hap.Characteristic.On, false);
         }
 
